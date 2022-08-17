ed4f340202079dfe33d78d79d84c5f9f8734e93d
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-17 23:53:09 +0300

    task4 added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Task 1");
         String firstName = "Семён";
         String middleName = "Семёнович";
-        String lastName = "Иванов";
+        String lastName = "  Семёнов";
         String fullName = String.join(" ", lastName, firstName, middleName);
         System.out.println("ФИО сотрудника — " + fullName);
 
@@ -15,5 +15,32 @@ public class Main {
         //Task3
         System.out.println("\nTask 3");
         System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
+
+        //Task4
+
+        System.out.println("\nTask 4");
+        fullName = fullName.trim(); // отбрасывем пустые символы в начале и в конце навсякий случай
+        fullName = fullName.replaceAll("\\s+", "#"); // заменяем пустоты между словами на '#'
+
+        //1 вариант
+        int firstIndex = fullName.indexOf('#'); // находим индекс '#', где заканчивается фамилия и начинается имя
+        int lastIndex = fullName.lastIndexOf('#'); // находим индекс '#', где кончается имя и начинается отчество
+        String lName = fullName.substring(0, firstIndex);
+        String fName = fullName.substring(firstIndex + 1, lastIndex);
+        String mName = fullName.substring(lastIndex + 1);
+
+        /*
+        //2 вариант
+        String[] flmNames = fullName.split("#");
+        lName = flmNames[0];
+        fName = flmNames[1];
+        mName = flmNames[2];
+        */
+
+        //печатаем
+        System.out.println("Имя сотрудника — " + fName);
+        System.out.println("Фамилия сотрудника — " + lName);
+        System.out.println("Отчество сотрудника — " + mName);
+
     }
 }
