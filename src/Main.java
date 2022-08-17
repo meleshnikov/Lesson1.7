4de004b92a9f96be187e3023495c39b8c52c012e
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-17 17:21:50 +0300

    task3 added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         //Task1
         System.out.println("Task 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
 
@@ -12,5 +12,8 @@ public class Main {
         System.out.println("\nTask 2");
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
+        //Task3
+        System.out.println("\nTask 3");
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
